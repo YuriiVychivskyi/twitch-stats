1bec65282b3f59a6b8fd72bfed82efae450e6105
Malberee <pavellyalkov45@gmail.com>
2026-02-20 18:34:36 +0200

    chore: install and configure ESLint

---
+++ client/eslint.config.js
@@ -0,0 +1,21 @@
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+import baseConfig from '../eslint-base.config.js'
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  ...baseConfig,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+
+export default eslintConfig
