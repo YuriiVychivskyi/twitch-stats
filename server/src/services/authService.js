b71ff0a554a2cb43ab58b883d0b827dedc496873
Yura Vyvhivskii <yuravychivskii@gmail.com>
2026-04-23 22:11:39 +0100

    feat: add twitch auth services

---
+++ server/src/services/authService.js
@@ -0,0 +1,40 @@
+import { validateTwitchEnv } from '../config/env.js'
+import { AppError } from '../utils/AppError.js'
+import { upsertStreamer } from './streamerService.js'
+
+const getTwitchAuthUrl = () => {
+  const url = new URL('https://id.twitch.tv/oauth2/authorize')
+
+  validateTwitchEnv()
+
+  const params = new URLSearchParams({
+    client_id: process.env.TWITCH_CLIENT_ID,
+    response_type: 'code',
+    scope: 'user:read:email',
+    redirect_uri: process.env.TWITCH_CALLBACK_URL,
+  })
+
+  return `${url.toString()}?${params.toString()}`
+}
+
+const handleTwitchCallback = async (code) => {
+  const url = new URL('https://id.twitch.tv/oauth2/token')
+
+  if (!code) throw new AppError('Code params error', 400)
+
+  validateTwitchEnv()
+
+  const params = new URLSearchParams({
+    client_id: process.env.TWITCH_CLIENT_ID,
+    client_secret: process.env.TWITCH_CLIENT_SECRET,
+    code: code,
+    grant_type: 'authorization_code',
+    redirect_uri: process.env.TWITCH_CALLBACK_URL,
+  })
+
+  const streamer = await upsertStreamer(url, params)
+
+  return streamer
+}
+
+export { getTwitchAuthUrl, handleTwitchCallback }
---
+++ server/src/services/twitchService.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+import { validateTwitchEnv } from '../config/env'
+import { AppError } from '../utils/AppError.js'
+
+export const getTwitchUserData = async (url, params) => {
+  const response = await axios.post(url, params)
+  const {
+    data: { access_token, refresh_token, scope, expires_in },
+    data,
+  } = response
+
+  if (!data || !access_token || !refresh_token)
+    throw new AppError('Authentication failed', 401)
+
+  const userToken = response.data.access_token
+
+  validateTwitchEnv()
+
+  const userResponse = await axios.get('https://api.twitch.tv/helix/users', {
+    headers: {
+      Authorization: `Bearer ${userToken}`,
+      'Client-Id': `${process.env.TWITCH_CLIENT_ID}`,
+    },
+  })
+
+  if (!userResponse.data?.data?.[0]?.id)
+    throw new AppError('User not found', 404)
+
+  const userData = userResponse.data?.data?.[0]
+
+  return { userData, access_token, refresh_token, scope, expires_in }
+}
