7cad0e2dc8daa1cb184ca3d8247e106e5aece998
Yurii Vychivskyi <yuravychivskii@gmail.com>
2026-07-20 19:13:58 +0100

    feat(server): prevent EventSub replay attacks

---
+++ server/src/middleware/preventEventSubReplay.js
@@ -0,0 +1,34 @@
+import { getRedis } from '../config/redis.js'
+import { AppError } from '../utils/AppError.js'
+
+export const preventEventSubReplay = async (req, res, next) => {
+  const messageId = req.headers['twitch-eventsub-message-id']
+  const messageTimestamp = Date.parse(
+    req.headers['twitch-eventsub-message-timestamp'],
+  )
+  const currentTimestamp = Date.now()
+
+  if (Number.isNaN(messageTimestamp))
+    throw new AppError('Invalid message timestamp', 403)
+
+  if (!messageId) throw new AppError('Missing required headers', 403)
+
+  const timeDifference = Math.abs(currentTimestamp - messageTimestamp)
+
+  if (timeDifference > 10 * 60 * 1000)
+    throw new AppError('EventSub message is too old', 403)
+
+  const redisClient = getRedis()
+
+  const key = `eventsub:message:${messageId}`
+  const result = await redisClient.set(key, 'processed', {
+    NX: true,
+    EX: 10 * 60,
+  })
+
+  if (result === null) {
+    return res.sendStatus(204)
+  }
+
+  next()
+}
--- server/src/routes/eventSubRotes.js
+++ server/src/routes/eventSubRotes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import { handleEventSubNotification } from '../controllers/eventSubController.js'
+import { preventEventSubReplay } from '../middleware/preventEventSubReplay.js'
 import { verifyEventSubSignature } from '../middleware/verifyEventSubSignature.js'
 import { asyncWrapper } from '../utils/asyncWrapper.js'
 
@@ -9,6 +10,7 @@ const router = express.Router()
 router.post(
   '/twitch/webhook',
   verifyEventSubSignature,
+  preventEventSubReplay,
   asyncWrapper(handleEventSubNotification),
 )
 
